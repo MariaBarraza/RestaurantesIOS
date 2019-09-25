85bed196ffbf6b2d109a6f7e85c725406c1e80d3
Fer <reikafer@gmail.com>
2019-09-25 14:09:16 -0700

    Initial Commit

---
+++ RestauranteIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestauranteIOS
+//
+//  Created by Alumno on 9/25/19.
+//  Copyright © 2019 fer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RestauranteIOSTests/RestauranteIOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  RestauranteIOSTests.swift
+//  RestauranteIOSTests
+//
+//  Created by Alumno on 9/25/19.
+//  Copyright © 2019 fer. All rights reserved.
+//
+
+import XCTest
+@testable import RestauranteIOS
+
+class RestauranteIOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
